156655e78e3cdd5286a785d4f38fe7867b8ce5da
Nathan Faria <nathanfaria607@gmail.com>
2023-06-01 16:13:26 -0300

    feat: add HttpGet all products in product controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APICatalog.Context;
+using APICatalog.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalog.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        public readonly APICatalogContext _context;
+        public ProductsController(APICatalogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var products = this._context.Products.ToList();
+            if(products == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
     }
 }
