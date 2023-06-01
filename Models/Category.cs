f3c912ec22ea98071c54ad679a97728218f9f0b5
Nathan Faria <nathanfaria607@gmail.com>
2023-06-01 11:10:58 -0300

    feat: add the relation 1-N between Category and products and map in the models

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,18 @@
-﻿namespace APICatalog.Models
+﻿using Microsoft.VisualBasic;
+using System.Collections.ObjectModel;
+
+namespace APICatalog.Models
 {
     public class Category
     {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
         public int CategoryId { get; set; }
         public string? Name { get; set; }
         public string? ImageUrl { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,12 +2,15 @@
 {
     public class Product
     {
-        public int Id { get; set; }
+        public int ProductId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
         public float Stock { get;set; }
         public DateTime RecordDate { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
     }
 }
