5a18344efd6140cf8e856d9faec6fac58fc1138f
Nathan Faria <nathanfaria607@gmail.com>
2023-06-01 10:45:33 -0300

    feat: add APICatalogContext with the DbSets of the models

---
+++ Context/APICatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalog.Context
+{
+    public class APICatalogContext : DbContext
+    {
+        public APICatalogContext(DbContextOptions<APICatalogContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
