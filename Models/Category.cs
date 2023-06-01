24143a2b4cef3df13c1486e0f0c6696432913a35
Nathan Faria <nathanfaria607@gmail.com>
2023-06-01 13:55:17 -0300

    feat: add DataAnnotations in the models to change the way the database is handling the properties

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalog.Models
 {
+    [Table("Category")]
     public class Category
     {
         public Category()
         {
             Products = new Collection<Product>();
         }
+        [Key]
         public int CategoryId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string? Name { get; set; }
+        [Required]
+        [StringLength(255)]
         public string? ImageUrl { get; set; }
 
         public ICollection<Product>? Products { get; set; }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,15 +1,29 @@
-﻿namespace APICatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalog.Models
 {
+    [Table("Product")]
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string? Name { get; set; }
+        [Required]
+        [StringLength(255)]
         public string? Description { get; set; }
+        [Required]
+        [Column(TypeName ="decimal(9,2)")]
         public decimal Price { get; set; }
+        [Required]
+        [StringLength(255)]
         public string? ImageUrl { get; set; }
         public float Stock { get;set; }
         public DateTime RecordDate { get; set; }
-
+        [Required]
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
     }
