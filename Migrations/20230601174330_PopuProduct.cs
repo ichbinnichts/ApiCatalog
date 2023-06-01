32c323bb4c56f22c42da1a21c1d29b475c6035b3
Nathan Faria <nathanfaria607@gmail.com>
2023-06-01 15:00:57 -0300

    feat: add PopuProduct migration with -dotnet ef migrations add PopuProduct- and add dml script in the Up and Down method

---
+++ Migrations/20230601174330_PopuProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopuProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Product(\"Name\", \"Description\", \"Price\", \"ImageUrl\", \"Stock\", \"RecordDate\", \"CategoryId\") VALUES('Beer', 'Alcoholic beverage', 2.50, 'Beer.jpg', 50, now(), 1)");
+
+            migrationBuilder.Sql("INSERT INTO Product(\"Name\", \"Description\", \"Price\", \"ImageUrl\", \"Stock\", \"RecordDate\", \"CategoryId\") VALUES('Vegan Sandwich', 'Sandwich with no meat', 1.20, 'VeganSandwich.jpg', 50, now(), 2)");
+
+            migrationBuilder.Sql("INSERT INTO Product(\"Name\", \"Description\"," +
+                " \"Price\", \"ImageUrl\", \"Stock\", \"RecordDate\", \"CategoryId\")" +
+                " VALUES('Pudding', 'Milk-based dessert', 2, 'Pudding.jpg', 50, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Product");
+        }
+    }
+}
